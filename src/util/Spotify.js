3d8b828e98cc3b288f98f3e822071fdbd1a89ae0
Asylbek Ismagambetov <ismagambetov@gmail.com>
2019-10-15 20:47:45 -0400

    steps 83/99 are completed

---
+++ src/util/Spotify.js
@@ -0,0 +1,29 @@
+const clientId = "66f08345d8f04327ab81ba60fe669488";
+const redirectUri = "http://localhost:3000/";
+let accessToken;
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+
+    // check for access token match
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expireInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expireInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expireIn = Number(expireInMatch[1]);
+      // This clears the parameters, allowing us to grab a new access token when it expires.
+      window.setTimeout(() => (accessToken = ""), expireIn * 1000);
+      window.history.pushState("Access Token", null, "/");
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      window.location = accessUrl;
+    }
+  }
+};
+
+export default Spotify;
