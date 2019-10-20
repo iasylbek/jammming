99d0e2d1a5da22d43731f04f674a577b7f59debc
Asylbek Ismagambetov <ismagambetov@gmail.com>
2019-10-20 18:16:47 -0400

    Finished 95/99 (mandatory steps), but still have some issues. Need to wait to resolve the issues.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -12,13 +12,13 @@ const Spotify = {
 
     // check for access token match
     const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
-    const expireInMatch = window.location.href.match(/expires_in=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
 
-    if (accessTokenMatch && expireInMatch) {
+    if (accessTokenMatch && expiresInMatch) {
       accessToken = accessTokenMatch[1];
-      const expireIn = Number(expireInMatch[1]);
+      const expiresIn = Number(expiresInMatch[1]);
       // This clears the parameters, allowing us to grab a new access token when it expires.
-      window.setTimeout(() => (accessToken = ""), expireIn * 1000);
+      window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
       window.history.pushState("Access Token", null, "/");
       return accessToken;
     } else {
@@ -46,6 +46,39 @@ const Spotify = {
           album: track.album.name,
           uri: track.uri
         }));
+      });
+  },
+
+  savePlaylist(name, trackUris) {
+    if (!name || !trackUris.length) {
+      return;
+    }
+
+    const accessToken = Spotify.getAccessToken();
+    const headers = { Authorization: `Bearer ${accessToken}` };
+    let userId;
+
+    return fetch("https://api.spotify.com/v1/me", { headers: headers })
+      .then(response => response.json())
+      .then(jsonResponse => {
+        userId = jsonResponse.id;
+        return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+          headers: headers,
+          method: "POST",
+          body: JSON.stringify({ name: name })
+        })
+          .then(response => response.json())
+          .then(jsonResponse => {
+            const playlistId = jsonResponse.id;
+            return fetch(
+              `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,
+              {
+                headers: headers,
+                method: "POST",
+                body: JSON.stringify({ uris: trackUris })
+              }
+            );
+          });
       });
   }
 };
